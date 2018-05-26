e732cfb63ea1ffb830cb35165cb7827f18269642
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-26 13:40:15 +0200

    Added PrimeNG and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { PanelModule } from 'primeng/components/panel/panel';
+import { ButtonModule } from 'primeng/components/button/button';
+import { RadioButtonModule } from 'primeng/components/radioButton/radioButton';
+import { FileUploadModule } from 'primeng/fileupload';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UploadImageComponent } from './components/upload-image/upload-image.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule,
+    RadioButtonModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
