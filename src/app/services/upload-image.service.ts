9339a0863ab802b63f78aa97663ccd24a2fb20e6
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-05-28 23:29:28 +0200

    removed services array from form data

--- src/app/services/upload-image.service.ts
+++ src/app/services/upload-image.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {API_BASE_URL} from '../constants';
 
 @Injectable()
@@ -8,12 +8,9 @@ export class UploadImageService {
 
   upload(image: File) {
     const data = new FormData();
-    data.set('file', image);
-    data.append('services[]', 'service');
-    data.append('services[]', 'service2');
-    data.append('services[]', 'service3');
+    data.append('file', image);
 
     this.http.post(`${API_BASE_URL}/upload`, data)
-      .subscribe((imageId: String) => console.log(imageId), err => console.error(err.error));
+      .subscribe((res: any) => console.log(res.imageId), err => console.error(err.error));
   }
 }
